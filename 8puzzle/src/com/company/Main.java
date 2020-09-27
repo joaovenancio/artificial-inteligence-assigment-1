9ff581fc18ae773658ec00bd787937d045ca374a
Joao Vitor Venancio <joaovitorvenancio99@gmail.com>
2020-09-27 16:09:56 -0300

    implemented the heuristics evaluation and the initial console window

--- 8puzzle/src/com/company/Main.java
+++ 8puzzle/src/com/company/Main.java
@@ -1,18 +1,49 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        boolean rodando = true;
+        Scanner input = new Scanner(System.in);
 
-        //EightPuzzle.getInstance().test();
-        //EightPuzzle.getInstance().resolveQ1(Node.testNode1);
-        //EightPuzzle.getInstance().resolveQ1(new int[][]{{1,2,0}, {4,5,6}, {8,7,3}});
-        //EightPuzzle.getInstance().resolveQ1(new int[][]{{8,0,4}, {5,3,2}, {7,6,1}});
-        EightPuzzle.getInstance().resolveQ1(Node.randomBoard());
-        //EightPuzzle.getInstance().resolveQ1(new int[][]{{1,2,3}, {4,0,6}, {5,7,8}});
-        //test lso
-        //joão é um banana
-        // vrauu
+        System.out.println("########################################################################");
+        System.out.println("#                               8Puzzle                                #");
+        System.out.println("########################################################################");
+
+
+        while (rodando) {
+            System.out.println("1 - Custo Uniforme (sem heurística)");
+            System.out.println("2 - A* com uma heurística simples");
+            System.out.println("3 - A* com a heurística mais precisa");
+            System.out.println("4 - Sair do programa");
+            System.out.println("Digite a opção desejada: ");
+            int in = input.nextInt();
+            switch (in) {
+                case 1:
+                    EightPuzzle.getInstance().resolveQ1(Node.testNode1);
+                    break;
+
+                case 2:
+                    EightPuzzle.getInstance().resolveQ2(Node.testNode1);
+                    break;
+
+                case 3:
+                    EightPuzzle.getInstance().resolveQ3(Node.testNode1);
+                    break;
+
+                case 4:
+                    rodando = false;
+                    break;
+
+                    default:
+                        System.out.println("Por favor, digite uma opção válida.");
+
+            }
+
+        }
+
 
     }
 }
